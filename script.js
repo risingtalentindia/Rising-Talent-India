07d0e10213d61bf67699acc77437eb7c2c6d2f21
risingtalentindia <risingtalentindia.official@gmail.com>
2026-08-20 11:02:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log(
         "Welcome to Rising Talent India 🇮🇳"
     );
-
+console.log("REGISTRATION SCRIPT LOADED");
 
     // ==========================================
     // REGISTRATION FORM SUBMISSION
